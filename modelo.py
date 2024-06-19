5c737f27348c4550f213380668c48a32113dfa91
daniel-rodri <162440520+daniel-rodri@users.noreply.github.com>
2024-06-19 08:37:46 -0300

    Add files via upload

--- modelo.py
+++ modelo.py
@@ -1,43 +1,48 @@
 class Programa:
     def __init__(self, nome, Ano):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.Ano = Ano
-        self.__likes = 0
+        self._likes = 0
 
     @property
     def nome(self):
-        return self.__nome
+        return self._nome
     
     @nome.setter 
     def nome (self, novo_nome):
-        self.__nome = novo_nome.title()
+        self._nome = novo_nome.title()
 
     @property
     def likes(self):
-        return self.__likes
+        return self._likes
 
     def dar_likes(self):
-        self.__likes += 1
+        self._likes += 1
+
+    def imprime(self):
+        print(f'{self.nome} - {self.ano} - {self._likes} likes')
 
 class Filme(Programa):
-    def __init__(self, nome, Ano):
-        self.__nome = nome.title()
-        self.Ano = Ano
-        self.__likes = 0
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
+        self._likes = 0
 
 class Serie(Programa):
-    def __init__(self, nome, Ano, temporadas):
-        self.__nome = nome.title()
-        self.Ano = Ano
+    def __init__(self, nome, ano, temporadas):
+        super().__init__(nome, ano)
         self.temporadas = temporadas
-        self.__likes = 0
+        self._likes = 0
 
   
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 vingadores.dar_likes()
-print(f'nome: {vingadores.nome} - Ano: {vingadores.Ano} - duraçao: {vingadores.duraçao} - likes: {vingadores.likes}')
 
 atlanta =Serie('atlanta', 2018, 2)
 atlanta.dar_likes()
 atlanta.dar_likes()
-print(f'nome: {atlanta.nome} - Ano: {atlanta.Ano} - temporadas: {atlanta.temporadas} - likes: {atlanta.likes}')
+
+filmes_e_series = [vingadores, atlanta]
+
+for programa in filmes_e_series:
+    programa.imprime()
